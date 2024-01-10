1f17378b5c8ddeda885b15847c1807904d672e90
Hazal OZER <hazalozer07@gmail.com>
2024-01-10 22:50:53 +0300

    added comment

--- src/week5/C05_P_DiziHatasi.java
+++ src/week5/C05_P_DiziHatasi.java
@@ -4,26 +4,27 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class C05_P_DiziHatasi {
-    //Tanımlanmış bir dizideki elemanları kullanıcıdan aldığımız veriyle döndüren metot yazılac
+    //Tanımlanmış bir dizideki elemanları kullanıcıdan aldığımız veriyle döndüren metot yazılacak
 
     public static void print(String[] arr) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Index: ");
+        Scanner scan = new Scanner(System.in);//Scanner çağırıldı
+        System.out.print("Index: ");//Kullanıcı ekranına mesaj gönderildi
         try {
-            int index = scan.nextInt();
-            System.out.println(arr[index]);
+            int index = scan.nextInt();//index kullanıcıdan alındı
+            System.out.println(arr[index]);//Eleman ekrana yazdırıldı
         } catch (InputMismatchException e) {
-            System.out.println("Lütfen tamsayı değeri giriniz");
+            System.out.println("Lütfen tamsayı değeri giriniz");//input hatası değerlendirildi
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Array 10 elemanlıdır. Lütfen 0'dan 10'a kadar bir sayı giriniz.");
+            System.out.println("Array 10 elemanlıdır. Lütfen 0'dan 10'a kadar bir sayı giriniz.");//dizi sınır değerlendirildi
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getMessage());//Kalan hatalar değerledirildi
         }
     }
 
+    //Main metot
     public static void main(String[] args) {
-        String[] arr = {"Kedi", "Köpek", "Kuş", "Balık", "Maymun", "Zürafa", "Gergedan", "Timsah", "Tavşan", "Yılan"};
-        print(arr);
+        String[] arr = {"Kedi", "Köpek", "Kuş", "Balık", "Maymun", "Zürafa", "Gergedan", "Timsah", "Tavşan", "Yılan"};//Dizi Manuel olarak tanımlandı
+        print(arr);//Print metodu çağırıldı. Dizi içine atıldı
 
 
     }
